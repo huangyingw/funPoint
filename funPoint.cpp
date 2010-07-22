9d5430efa9f00c1043fffaf9c1456a7b93477ec7
huangyingw@linux_programming <huangyingw@gmail.com>
2010-07-22 21:38:44 +0800

    n

--- funPoint.cpp
+++ funPoint.cpp
@@ -7,8 +7,8 @@ int test(int a);
 int main(int argc,char* argv[])    
 {
   cout<<test<<endl;  
-  typedef int (*fp)(int a);//注意,这里不是生命函数指针,而是定义一个函数指针的类型,这个类型是自己定义的,类型名为fp
-  fp fpi;//这里利用自己定义的类型名fp定义了一个fpi的函数指针!
+  typedef int (*fp)(int a);
+  fp fpi;
   fpi=test;
   cout<<fpi(5)<<"|"<<(*fpi)(10)<<endl;
 	int (*myp)(int a)=test;
