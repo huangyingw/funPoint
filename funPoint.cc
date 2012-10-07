f9cf23787f0ebe7032476d263bd2d553d8889fb4
huangyingw@linux_programming <huangyingw@gmail.com>
2012-10-07 17:58:23 -0500

    n

--- funPoint.cc
+++ funPoint.cc
@@ -2,7 +2,38 @@
 #include <iostream>
 using namespace std;
 int test(int a);
-int main(int argc,char* argv[])    {
+/*
+int main(int argc,char* argv[])  {
+  return 0;
+}
+*/
+int test(int a) {
+  return a;
+} 
+class A {
+  public:
+  int fun1(int i) {
+  return i;
+  };
+};
+template <class T>
+void fun2(int j,T *obp,int (T::*p)(int)) {
+  cout <<(obp->*p)(j);
+}
+void fun2(int j,int (*p)(int)) {
+  cout<<p(j);
+}
+int fun3(int i) {
+  return i;
+}
+int main() {
+  int (A::*fp1)(int);
+  fp1=&A::fun1;
+  A oba;
+  A *obap=&oba;
+  int i=1;
+  fun2(i,obap,fp1);
+  fun2(i,fun3);
   cout<<test<<endl;  
   typedef int (*fp)(int a);
   fp fpi;
@@ -13,6 +44,3 @@ int main(int argc,char* argv[])    {
   cout<<myp(100)<<endl;
   return 0;
 }
-int test(int a) {
-  return a;
-} 
